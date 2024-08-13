628868ce0cd446064e4e98741034b8bc0d5f464f
sannechka <am.2511@yandex.ru>
2024-08-13 20:31:51 +0300

    added links

--- src/deployments-list.tsx
+++ src/deployments-list.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
-import { Flex, useColorModeValue} from '@chakra-ui/react'
-import { Box, Text} from '@chakra-ui/react'
+import {Flex, Link, useColorModeValue} from '@chakra-ui/react'
+import {Box, Text} from '@chakra-ui/react'
 import {Deployment, useGetDeploymentsQuery} from "./store/endpoints/be.endpoints";
 
 
-
-const DeploymentsList = ({onClose, ...rest}: SidebarProps) => {
+const DeploymentsList = () => {
 
     const {data: deployments = [], isLoading} = useGetDeploymentsQuery();
     const deployments1 = [{name: "deploy 1. ", status: "TEMP"}, {
@@ -21,7 +20,7 @@ const DeploymentsList = ({onClose, ...rest}: SidebarProps) => {
             borderRightColor={useColorModeValue('gray.200', 'gray.700')}
             w={{md: 60}}
             h="full"
-            {...rest}>
+        >
             <Flex h="12" alignItems="center" mx="8" justifyContent="space-between">
                 <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold">
                     Deployments
@@ -29,8 +28,10 @@ const DeploymentsList = ({onClose, ...rest}: SidebarProps) => {
             </Flex>
             <Flex direction={"column"} gap={2} padding={5} paddingTop={0}>
                 {deployments.map((deployment: Deployment) => (
-                    <Flex gap ={5}>
-                        <div>{deployment.projectName}</div>
+                    <Flex gap={5} justifyContent={'space-between'}>
+                        <Link href={deployment.gitlabLink} isExternal>
+                            {deployment.projectName}
+                        </Link>
                         <div>{deployment.state}</div>
                     </Flex>
                 ))}
--- src/features/project/projects-table.tsx
+++ src/features/project/projects-table.tsx
@@ -1,17 +1,19 @@
 import '../../App.css';
 import {
-    IconButton, Stack, Table, TableContainer, Tbody, Td, Th, Thead, Tr,
+    IconButton, Link, Stack, Table, TableContainer, Tbody, Td, Th, Thead, Tr,
 } from '@chakra-ui/react'
 import {Project, useGetProjectsQuery} from "../../store/endpoints/be.endpoints";
 import {SearchIcon} from '@chakra-ui/icons'
 import {useNavigate} from "react-router-dom";
 import SubmitDeployPopup from "../deploy/submit-deploy-popup";
 import EditProjectPopup from "./edit-project-popup";
+import {ReactNode} from "react";
 
 
 type TableColumn = {
     key: string;
     label: string;
+    render?: (value: string) => ReactNode
 }
 const columns: TableColumn[] = [
     {
@@ -24,11 +26,13 @@ const columns: TableColumn[] = [
     },
     {
         key: 'gitConfigRepository',
-        label: 'Config Repository'
+        label: 'Config Repository',
+        render: (value: string) => <Link color="teal.500" href={value} isExternal>{value}</Link>
     },
     {
         key: 'artifactoryDeploymentDescriptorFolder',
-        label: 'Artifactory Descriptor Folder'
+        label: 'Artifactory Descriptor Folder',
+        render: (value: string) => <Link color="teal.500" href={value} isExternal>{value}</Link>
     },
     {
         key: 'services',
@@ -71,10 +75,14 @@ const ProjectsTable = () => {
                         <>
                             {projects?.map(project => <Tr>
                                 {columns.map(column => <Td
-                                    key={project.id + column.key}>{project[column.key as keyof Project]}</Td>)}
+                                    key={project.id + column.key}>
+                                    {column.render ?
+                                        column.render(project[column.key as keyof Project])
+                                        : project[column.key as keyof Project]}
+                                </Td>)}
                                 <Stack direction='row' spacing={2}>
                                     <EditProjectPopup/>
-                                    <IconButton icon={<SearchIcon/>} onClick={navigateToEnvs}/>
+                                    <IconButton aria-label={'envs'} icon={<SearchIcon/>} onClick={navigateToEnvs}/>
                                     <SubmitDeployPopup/>
                                 </Stack>
                             </Tr>)}
